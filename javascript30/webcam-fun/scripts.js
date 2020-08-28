ffc88ab5b2b7f240f87225a9ca1f8bce58a6387e
Kayla Jean Crane <kayla.crane@gmail.com>
2020-08-28 18:06:51 +0200

    add additional filters to webcam exercise

--- javascript30/webcam-fun/scripts.js
+++ javascript30/webcam-fun/scripts.js
@@ -3,6 +3,8 @@ const canvas = document.querySelector(".photo");
 const ctx = canvas.getContext("2d");
 const strip = document.querySelector(".strip");
 const snap = document.querySelector(".snap");
+const filters = document.querySelectorAll(".filters input[type=radio]");
+let appliedFilter = "noFilter";
 
 function getVideo() {
   navigator.mediaDevices
@@ -16,6 +18,9 @@ function getVideo() {
       console.error("Oh no!", error);
     });
 }
+let isSplitOn = false;
+let isRedOn = false;
+let isGreenscreenOn = false;
 
 function paintToCanvas() {
   const width = video.videoWidth;
@@ -25,14 +30,29 @@ function paintToCanvas() {
 
   return setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height);
-    // take the pixels out
+    ctx.filter = "none";
     let pixels = ctx.getImageData(0, 0, width, height);
-    // change their
-    // pixels = redEffect(pixels);
-    // pixels = rgbSplit(pixels);
+    if (appliedFilter == "noFilter") {
+      return;
+    } else if (appliedFilter === "redFilter") {
+      pixels = redEffect(pixels);
+    } else if (appliedFilter === "splitFilter") {
+      pixels = rgbSplit(pixels);
+    } else if (appliedFilter === "greyFilter") {
+      // pixels = greyEffect(pixels);
+      ctx.filter = "grayscale(100%";
+    } else if (appliedFilter === "greenscreenFilter") {
+      pixels = greenScreen(pixels);
+    } else if (appliedFilter === "blurFilter") {
+      ctx.filter = "blur(4px)";
+    } else if (appliedFilter === "invertFilter") {
+      ctx.filter = "invert(100%)";
+    } else if (appliedFilter === "sepiaFilter") {
+      ctx.filter = "sepia(100%)";
+    }
     ctx.globalAlpha = 0.8; /*creates a ghost effect */
 
-    pixels = greenScreen(pixels);
+    // ;
     // put them back
     ctx.putImageData(pixels, 0, 0);
   }, 16);
@@ -75,6 +95,17 @@ function rgbSplit(pixels) {
   return pixels;
 }
 
+// function greyEffect(pixels) {
+//   for (let i = 0; i < pixels.data.length; i += 4) {
+//     const average =
+//       (pixels.data[i + 0] + pixels.data[i + 1] + pixels.data[i + 2]) / 3;
+//     pixels.data[i + 0] = average; // RED
+//     pixels.data[i + 1] = average; // GREEN
+//     pixels.data[i + 2] = average; // Blue
+//   }
+//   return pixels;
+// }
+
 function greenScreen(pixels) {
   const levels = {};
 
@@ -82,11 +113,11 @@ function greenScreen(pixels) {
     levels[input.name] = input.value;
   });
 
-  for (i = 0; i < pixels.data.length; i = i + 4) {
-    red = pixels.data[i + 0];
-    green = pixels.data[i + 1];
-    blue = pixels.data[i + 2];
-    alpha = pixels.data[i + 3];
+  for (let i = 0; i < pixels.data.length; i = i + 4) {
+    let red = pixels.data[i + 0];
+    let green = pixels.data[i + 1];
+    let blue = pixels.data[i + 2];
+    let alpha = pixels.data[i + 3];
 
     if (
       red >= levels.rmin &&
@@ -103,6 +134,9 @@ function greenScreen(pixels) {
 
   return pixels;
 }
+function filterHandler(event) {
+  appliedFilter = event.currentTarget.id;
+}
 
 getVideo();
 
@@ -110,3 +144,5 @@ video.addEventListener(
   "canplay",
   paintToCanvas
 ); /* this listen to video and when it can play it paints the video to the canvas */
+
+filters.forEach((filter) => filter.addEventListener("change", filterHandler));
