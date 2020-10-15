8d1021bf565845a56383ff7823ad4c4ce9f9fb12
Kayla Jean Crane <kayla.crane@gmail.com>
2020-10-15 14:07:52 +0200

    add objects - properties, attributes, accessor props

--- advanced-obj-oriented-javascript-udemy/functions/main.js
+++ advanced-obj-oriented-javascript-udemy/functions/main.js
@@ -1,13 +1,47 @@
-/*hoisting works with function declaration*/
-multiplyNumbers(2, 30);
-/*hoisting doesn't work with function expression*/
-multiplyTwoNums(5, 10);
+// /*hoisting works with function declaration*/
+// multiplyNumbers(2, 30);
+// /*hoisting doesn't work with function expression*/
+// multiplyTwoNums(5, 10);
 
-/*function declaration*/
-function multiplyNumbers(a, b) {
-  console.log(a * b);
-}
-/*function expression*/
-const multiplyTwoNums = function () {
-  console.log(a * b);
+// /*function declaration*/
+// function multiplyNumbers(a, b) {
+//   console.log(a * b);
+// }
+// /*function expression*/
+// const multiplyTwoNums = function () {
+//   console.log(a * b);
+// };
+
+const person = {
+  first: "Jill",
+  last: "Howlett",
+  birthYear: 2000,
+  calculateAge(currentYear) {
+    console.log(currentYear - this.birthYear);
+  },
+  get wholeName() {
+    console.log(`${this.first} ${this.last}`);
+  },
+  set wholeName(newValue) {
+    const parts = newValue.split(" ");
+    this.first = parts[0];
+    this.last = parts[1];
+  },
 };
+/* object method*/
+person.calculateAge(2020);
+
+/*bind the function*/
+function fullName() {
+  console.log(`${this.first} ${this.last}`);
+}
+
+let displayFull = fullName.bind(person);
+displayFull();
+console.log(person);
+
+/*getter/setter removes need to put () and allows for updating*/
+console.log(person);
+person.wholeName;
+person.wholeName = "Kayla Crane";
+console.log(person);
